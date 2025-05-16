555fec73cc857717d038f40f55b8d308ebc865d2
wuessslima <lim24012@byui.edu>
2025-05-16 15:31:43 -0300

    product.js bug

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,10 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const currentCart = getLocalStorage("so-cart") || [];
+  let currentCart = getLocalStorage("so-cart");
+  if (!Array.isArray(currentCart)) {
+    currentCart = [];
+  }
   currentCart.push(product);
   setLocalStorage("so-cart", currentCart);
 }
