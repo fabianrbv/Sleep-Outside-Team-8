681ac8d722a7325a742d08ab6203cb01176f1593
Fabian Betancourt <fabianrbv@gmail.com>
2025-05-22 18:41:23 -0500

    Activity 1

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -16,3 +22,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+getParam;
