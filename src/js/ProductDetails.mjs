b2a0dea4e82ef362ed760ac823f28f1b62e8485b
Fabian Betancourt <fabianrbv@gmail.com>
2025-06-07 22:06:38 -0500

    activity 3

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -18,11 +18,7 @@ export default class ProductDetails {
 
   addProductToCart() {
     const cartItems = getLocalStorage("so-cart") || [];
-    const productToAdd = {
-    ...this.product,
-    id: `${this.product.Name}-${Date.now()}`
-  };
-    cartItems.push(productToAdd);
+    cartItems.push(this.product);
     setLocalStorage("so-cart", cartItems);
   }
 
@@ -32,29 +28,48 @@ export default class ProductDetails {
 }
 
 function productDetailsTemplate(product) {
-  document.querySelector("h2").textContent = product.Brand.Name;
-  document.querySelector("h3").textContent = product.NameWithoutBrand;
+  document.querySelector("h2").textContent = product.Category.charAt(0).toUpperCase() + product.Category.slice(1);
+  document.querySelector("#p-brand").textContent = product.Brand.Name;
+  document.querySelector("#p-name").textContent = product.NameWithoutBrand;
 
-  const productImage = document.getElementById("productImage");
-  productImage.src = product.Image;
+  const productImage = document.querySelector("#p-image");
+  productImage.src = product.Images.PrimaryExtraLarge;
   productImage.alt = product.NameWithoutBrand;
+  const euroPrice = new Intl.NumberFormat("de-DE",
+    {
+      style: "currency", currency: "EUR",
+    }).format(Number(product.FinalPrice) * 0.85);
+  document.querySelector("#p-price").textContent = `${euroPrice}`;
+  document.querySelector("#p-color").textContent = product.Colors[0].ColorName;
+  document.querySelector("#p-description").innerHTML = product.DescriptionHtmlSimple;
 
-  document.getElementById("productPrice").textContent = product.FinalPrice;
-  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
-  document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
-
-  document.getElementById("addToCart").dataset.id = product.Id;
-
-    // discount 
-  const original = product.SuggestedRetailPrice;
-  const current = product.FinalPrice;
-
-  if (original > current) {
-    const discountPercent = Math.round(((original - current) / original) * 100);
-    const discountBadge = document.createElement("span");
-    discountBadge.textContent = `${discountPercent}% OFF`;
-
-    const priceElement = document.getElementById("productPrice");
-    priceElement.insertAdjacentElement("afterend", discountBadge);
-     }
+  document.querySelector("#add-to-cart").dataset.id = product.Id;
 }
+
+// function productDetailsTemplate(product) {
+//   document.querySelector("h2").textContent = product.Brand.Name;
+//   document.querySelector("h3").textContent = product.NameWithoutBrand;
+
+//   const productImage = document.getElementById("productImage");
+//   productImage.src = product.Image;
+//   productImage.alt = product.NameWithoutBrand;
+
+//   document.getElementById("productPrice").textContent = product.FinalPrice;
+//   document.getElementById("productColor").textContent = product.Colors[0].ColorName;
+//   document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
+
+//   document.getElementById("addToCart").dataset.id = product.Id;
+
+//     // discount 
+//   const original = product.SuggestedRetailPrice;
+//   const current = product.FinalPrice;
+
+//   if (original > current) {
+//     const discountPercent = Math.round(((original - current) / original) * 100);
+//     const discountBadge = document.createElement("span");
+//     discountBadge.textContent = `${discountPercent}% OFF`;
+
+//     const priceElement = document.getElementById("productPrice");
+//     priceElement.insertAdjacentElement("afterend", discountBadge);
+//      }
+// }
--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -19,9 +19,10 @@ export default class ProductList {
     this.listElement = listElement;
   }
 
-  async init() {
-    const list = await this.dataSource.getData();
+async init() {
+    const list = await this.dataSource.getData(this.category);
     this.renderList(list);
+    document.querySelector(".title").textContent = this.category;
   }
 
   renderList(list) {
