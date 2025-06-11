a5e78c01972e6b1bbf0b7ccb2182f433b611a5d8
Fabian Betancourt <fabianrbv@gmail.com>
2025-06-11 02:51:02 -0500

    Done

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,26 +1,28 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-async function convertToJson(res) {
-  const jsonResponse = await res.json();
+function convertToJson(res) {
   if (res.ok) {
-    return jsonResponse;
+    return res.json();
   } else {
-    throw { name: "servicesError", message: jsonResponse };
+    throw new Error("Bad Response");
   }
 }
 
 export default class ExternalServices {
-  constructor() {}
-
+  constructor() {
+    // this.category = category;
+    // this.path = `../public/json/${this.category}.json`;
+  }
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
+    
     return data.Result;
   }
-
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
+    // console.log(data.Result);
     return data.Result;
   }
 
@@ -32,7 +34,6 @@ export default class ExternalServices {
       },
       body: JSON.stringify(payload),
     };
-    const response = await fetch(`${baseURL}checkout/`, options);
-    return convertToJson(response);
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
   }
 }
