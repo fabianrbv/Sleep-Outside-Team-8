0e8a3d16d5545d79fabeea7eb4b3a6cf1bfb0d8f
Fabian Betancourt <fabianrbv@gmail.com>
2025-06-11 01:08:21 -0500

    page working2.0

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -32,7 +32,10 @@ export default class ProductList {
     async init() {
     const list = await this.dataSource.getData(this.category);
     this.renderList(list);
-    document.querySelector(".title").textContent = this.category;
+    const titleElement = document.querySelector(".title");
+    if (titleElement) {
+      titleElement.textContent = this.category;
+}
   }
 
 }
